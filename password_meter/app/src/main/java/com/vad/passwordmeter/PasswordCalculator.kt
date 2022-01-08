392288a05f965ed45d1c339a6252fc6cbc8c3e4b
Liem Vo <liemvt@gmail.com>
2022-01-09 01:31:06 +1300

    Udpate repose

--- password_meter/app/src/main/java/com/vad/passwordmeter/PasswordCalculator.kt
+++
@@ -1,8 +0,0 @@
-package com.vad.passwordmeter
-
-interface PasswordCalculator {
-    val minimumLength: Int
-        get() = 8
-
-    fun calculateSecurityLevel(password: String?) : SecurityLevel
-}
